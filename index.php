e9fa7205e4f755520a2221a85352b5530bd1701c
Dagaflux <dagaflux>
2018-02-10 11:36:40 +0100

    branch init

---
+++ index.php
@@ -0,0 +1,14 @@
+ <!DOCTYPE html>
+<html>
+	<head>
+		<title>Portfolio - François Bouhours</title>
+		<link rel="stylesheet" type="text/css" href="css/styles.css">
+	</head>
+
+	<body>
+		<div class="page-wrapper">
+
+
+		</div>
+	</body>
+</html>
