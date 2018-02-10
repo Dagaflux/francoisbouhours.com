ce9d673156a1befbcbd2c481ddbed8ea43ea21ee
Dagaflux <dagaflux>
2018-02-10 14:17:19 +0100

    public files

--- index.php
+++ index.php
@@ -2,13 +2,22 @@
 <html>
 	<head>
 		<title>Portfolio - François Bouhours</title>
-		<link rel="stylesheet" type="text/css" href="css/styles.css">
+		<link href="https://fonts.googleapis.com/css?family=Open+Sans|Yellowtail" rel="stylesheet"> 
+		<link href="https://use.fontawesome.com/releases/v5.0.6/css/fontawesome.css" rel="stylesheet">
+		<link rel="stylesheet" type="text/css" href="public/stylesheets/styles.css">
 	</head>
 
 	<body>
 		<div class="page-wrapper">
+<i class="fab fa-linkedin-in"></i>
+<i class="fab fa-viadeo"></i>
+
+<i class="fas fa-envelope"></i>
+
+
+
 
 
 		</div>
-	</body>
+		</body>
 </html>
